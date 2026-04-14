a3e30a426c5dcb2c6681080b9cc61c794045ebf2
Daniyar <104356374+chaze77@users.noreply.github.com>
2026-04-14 13:42:59 +0600

    add query filter by acrive banners, blog

--- web/lib/apis/blogs.ts
+++ web/lib/apis/blogs.ts
@@ -2,15 +2,17 @@ import { IBlog } from '@/common/types';
 import { fetchDocuments, getDocumentById } from './api';
 import { appwriteKeys } from '@/appwrite/environment';
 import { cacheLife } from 'next/cache';
+import { Query } from 'appwrite';
 
 export const getBlogs = async (): Promise<IBlog[] | null> => {
   'use cache';
   cacheLife('days');
-
+  const query = [Query.equal('active', true)];
   try {
     const response = await fetchDocuments(
       appwriteKeys.DATABASE_ID,
       appwriteKeys.BLOG_ID,
+      query,
     );
     return response as IBlog[];
   } catch (error) {
